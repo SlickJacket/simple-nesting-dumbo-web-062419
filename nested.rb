3fcc63da117d7e4362625accb87bff8b0e83055d
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-17 05:05:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 return programmer_hash[:dennis_ritchie][:languages]
+		 string = programmer_hash[:dennis_ritchie][:languages]
+		 return string
 end
 
 def adding_matz
